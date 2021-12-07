4aa9ed7c319bff200dde35a1a0c8b9501070ec97
Tyler Nelson <tyler@mediaslave.com>
2021-12-07 15:16:31 -0500

    First commit

---
+++ src/main/java/me/helloweird/apps/weirdo/WeirdoApplication.java
@@ -0,0 +1,13 @@
+package me.helloweird.apps.weirdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeirdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeirdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/helloweird/apps/weirdo/WeirdoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.helloweird.apps.weirdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeirdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
